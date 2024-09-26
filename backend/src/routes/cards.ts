6e6e8cabe45db413690dd1816ceaf8713014e347
Veronica Nutting <veronicanutting@Veronicas-New-MacBook-Air.local>
2024-09-26 01:54:12 -0700

    bless linting

--- backend/src/routes/cards.ts
+++ backend/src/routes/cards.ts
@@ -4,29 +4,28 @@ import Card from '../models/card.model';
 const router = express.Router();
 
 interface CardType {
-    company: string;
-    type: string;
+  company: string;
+  type: string;
+}
+
+router.route('/companies').get(async (req, res) => {
+  try {
+    const companies = await Card.distinct('company');
+    res.json(companies);
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
   }
+});
 
-
-  router.route('/companies').get(async (req, res) => {
-    try {
-      const companies = await Card.distinct('company');
-      res.json(companies);
-    } catch (err) {
-      res.status(400).json('Error: ' + err);
-    }
-  });
-  
-  router.route('/options/:company').get(async (req, res) => {
-    const { company } = req.params;
-    try {
-      const options = await Card.find({ company }).distinct('type');
-      res.json(options);
-    } catch (err) {
-      res.status(400).json('Error: ' + err);
-    }
-  });
+router.route('/options/:company').get(async (req, res) => {
+  const { company } = req.params;
+  try {
+    const options = await Card.find({ company }).distinct('type');
+    res.json(options);
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
+});
 
 router.route('/').get((req, res) => {
   Card.find()
@@ -43,35 +42,35 @@ router.route('/add').post((req, res) => {
 });
 
 router.route('/filter').post(async (req, res) => {
-    const { cards, categories }: { cards: CardType[], categories: string[] } = req.body;
-    const queries = cards.map((card: CardType) => ({ company: card.company, type: card.type }));
-  
-    try {
-        const cardTypes = await Card.find({ $or: queries });
-    
-        const bestCards = categories.map(category => {
-          let bestCard = null;
-          let highestPercentage = 0;
-    
-          cardTypes.forEach(card => {
-            const cashbackCategory = card.cashbackCategories.find(c => c.category === category);
-            if (cashbackCategory && cashbackCategory?.percentage && cashbackCategory?.percentage > highestPercentage) {
-              highestPercentage = cashbackCategory.percentage;
-              bestCard = {
-                company: card.company,
-                type: card.type,
-                percentage: cashbackCategory.percentage
-              };
-            }
-          });
-    
-          return { category, bestCard };
-        });
-  
-      res.json(bestCards);
-    } catch (err) {
-      res.status(400).json('Error: ' + err);
-    }
-  });
+  const { cards, categories }: { cards: CardType[], categories: string[] } = req.body;
+  const queries = cards.map((card: CardType) => ({ company: card.company, type: card.type }));
+
+  try {
+    const cardTypes = await Card.find({ $or: queries });
+
+    const bestCards = categories.map(category => {
+      let bestCard = null;
+      let highestPercentage = 0;
+
+      cardTypes.forEach(card => {
+        const cashbackCategory = card.cashbackCategories.find(c => c.category === category);
+        if (cashbackCategory && cashbackCategory?.percentage && cashbackCategory?.percentage > highestPercentage) {
+          highestPercentage = cashbackCategory.percentage;
+          bestCard = {
+            company: card.company,
+            type: card.type,
+            percentage: cashbackCategory.percentage
+          };
+        }
+      });
+
+      return { category, bestCard };
+    });
+
+    res.json(bestCards);
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
+});
 
 export default router;
