eda528f6566d38ac41598849524a23e280514779
Veronica Nutting <veronicanutting@Veronicas-New-MacBook-Air.local>
2024-10-21 16:03:03 -0700

    update api base url

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5001/api';
 
 export const fetchCardCompanies = async () => {
   try {
