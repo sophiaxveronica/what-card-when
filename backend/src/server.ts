23b61c88ea80c6b1419329c013a8cbd386860580
Veronica Nutting <veronicanutting@Veronicas-New-MacBook-Air.local>
2024-11-02 13:13:45 -0700

    fix: add additional debugging info for mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,6 +17,8 @@ app.use('/api/emails', emailsRouter);
 app.use(express.static('../dist'))
 
 mongoose.connect(process.env.MONGODB_URI as string);
+mongoose.set('debug', true);
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
